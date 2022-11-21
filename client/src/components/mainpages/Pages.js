82d51a1eebb290c4054f1a09a3218bca9708aa83
Tom <ffrancois737@gmail.com>
2022-11-21 16:13:17 +0800

    product view

--- client/src/components/mainpages/Pages.js
+++ client/src/components/mainpages/Pages.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Routes, Route} from 'react-router-dom'
 import Products from './products/Products'
+import DetailProduct from './detailProduct/DetailProduct'
 import Login from './auth/Login'
 import Register from './auth/Register'
 import Cart from './cart/Cart'
@@ -9,6 +10,7 @@ function Pages() {
   return (
     <Routes>
         <Route path='/' element={<Products/>}/>
+        <Route path='/detail/:id' element={<DetailProduct/>}/>
         <Route path='/login' element={<Login/>}/>
         <Route path='/register' element={<Register/>}/>
         <Route path='/cart' element={<Cart/>}/>
--- client/src/components/mainpages/products/Products.js
+++ client/src/components/mainpages/products/Products.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import {GlobalState} from '../../../GlobalState' 
+import ProductItem from '../utils/productitem/ProductItem'
 
-function products() {
+function Products() {
+  const state = useContext(GlobalState)
+  const [products] = state.productsAPI.products
+  // console.log(products)
+
   return (
-    <div>Product list</div>
+    <div className='products'>
+      {
+        products.map(product =>{
+          return <ProductItem key={product._id} product={product}/>
+        })
+      }
+    </div>
   )
 }
 
-export default products
+export default Products
