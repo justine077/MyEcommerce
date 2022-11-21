98c1a00d807ea4c05c9505a680619a43feab4bb3
Tom <ffrancois737@gmail.com>
2022-11-21 12:22:19 +0800

    client1

---
+++ client/src/components/mainpages/Pages.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import Products from './products/Products'
+import Login from './auth/Login'
+import Register from './auth/Register'
+import Cart from './cart/Cart'
+import NotFound from './utils/NotFound/NotFound'
+function Pages() {
+  return (
+    <Routes>
+        <Route path='/' element={<Products/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/register' element={<Register/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+
+        <Route path='*' element={<NotFound/>} />
+    </Routes>
+    
+  )
+}
+
+export default Pages
---
+++ client/src/components/mainpages/cart/Cart.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function cart() {
+  return (
+    <div>Cart Component</div>
+  )
+}
+
+export default cart
