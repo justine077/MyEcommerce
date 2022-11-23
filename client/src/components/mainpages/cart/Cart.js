3b043f68aa040d479c8fc4b7f18d28cf72da8cba
Tom <ffrancois737@gmail.com>
2022-11-23 16:39:54 +0800

    cart total edited

--- client/src/components/mainpages/cart/Cart.js
+++ client/src/components/mainpages/cart/Cart.js
@@ -53,7 +53,7 @@ function Cart() {
   const removeProduct = id =>{
     if(window.confirm("Do you want to delete this product?")){
       cart.forEach((item, index) => {
-        if(item._id == id ){
+        if(item._id === id ){
           cart.splice(index, 1)
         }
       })
